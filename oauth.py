b18a9f13212d95914409091eccbb932cf0bb8d65
Adam <134978231+sullynumber9@users.noreply.github.com>
2024-05-18 14:19:30 -0400

    code somewhat working

--- oauth.py
+++ oauth.py
@@ -1,15 +1,32 @@
-import secrets
 from flask import Flask, redirect, url_for
 from flask_dance.contrib.google import make_google_blueprint, google
+import json
+import os
+
+# setting env variables
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
+
+# getting the user info from the config files
+
+with open("config.json") as f:
+    config_data = json.load(f)
 
 app = Flask(__name__)
+# random thing
 app.secret_key = "supersekrit"
-blueprint = make_google_blueprint(
-    client_id = {{secrets.CLIENT_ID}},
-    client_secret={{secrets.client_secret}},
-    scope=["profile", "email"]
 
-)
+# setting client id and client secret
+
+client_id = config_data["client_id"]
+client_secret = config_data["client_secret"]
+
+print(client_id)
+print(client_secret)
+# another random thing
+scope=["profile", "email"]
+
+blueprint = make_google_blueprint(client_id, client_secret)
 
 app.register_blueprint(blueprint, url_prefix="/login")
 
@@ -17,7 +34,7 @@ app.register_blueprint(blueprint, url_prefix="/login")
 def index():
     if not google.authorized:
         return redirect(url_for("google.login"))
-    resp = google.get("/oauth2/v2/userinfo")
+    resp = google.get("/login/google/authorized")
     assert resp.ok, resp.text
     return "You are {email} on Google".format(email=resp.json()["email"])
 
